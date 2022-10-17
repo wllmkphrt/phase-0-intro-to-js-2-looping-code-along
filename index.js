fff198c071a68976f807fd06700b9184fedab9c9
William Kay Kephart <wllmkphrt@gmail.com>
2022-10-17 15:00:10 -0400

    commit tree 7873507967e9fd151092382adaf9f708b908700d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(array){
+    const newArr = [];
+    for(let i = 0; i < array.length; i++){
+        newArr[i] = `Thank you, ${array[i]}, for the wonderful surprise gift!`;
+    }
+    return newArr;
+}
+function countDown(num){
+    for(let i = num; i >= 0; i--){
+        console.log(i);
+    }
+}
